cdf5da4451fb2f21a41d3e061ddef2f5d28a1d0e
Motiejus Šveikauskas <118689022+Motiejukaas@users.noreply.github.com>
2025-09-23 09:54:58 +0300

    Hash algorithm selection

--- custom-hash/Hash/Hash.cs
+++ custom-hash/Hash/Hash.cs
@@ -4,6 +4,13 @@ public class Hash
 {
     public byte[] HashV01(byte[] input)
     {
+        //placeholder
+        return input;
+    }
+    
+    public byte[] HashV02(byte[] input)
+    {
+        //placeholder
         return input;
     }
 }
